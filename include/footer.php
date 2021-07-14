37ce56110d369cbb8f41a28e8bfa82ab0f6a1249
yukimitsu16 <87399914+yukimitsu16@users.noreply.github.com>
2021-07-14 14:36:02 +0900

    initial practice
    練習

---
+++ include/footer.php
@@ -0,0 +1,33 @@
+<footer>
+    <div class="bottom">
+    <ul>
+      <li><p>サイトマップ</p></li>
+        <li>農園について</li>
+        <li>栽培方法</li>
+        <li>クラウドファンディング</li>
+        <li>歴史</li>
+        <li>発足経緯</li>
+      </ul>
+      <ul>
+        <li>ECサイト</li>
+        <li>種類販売</li>
+      </ul>
+      <ul>
+        <li>調理例</li>
+        <li>惣菜</li>
+        <li>お菓子</li>
+        <li>調味料</li>
+      </ul>
+      <ul>
+        <li>お知らせ</li>
+        <li>直販売</li>
+        <li>提携レストラン</li>
+        <li>Blog</li>
+      </ul>
+  </div>
+      <small>
+      <p>商品取引商法</p>
+      <p>プライバシーポリシー</p>
+      <p>コピーライト</p>
+      </small>
+  </footer>
---
+++ include/nav.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="../css/reset.css">
+<link rel="stylesheet" href="../css/index.css">
+<nav>
+    <ul>
+      <li><a href="../index.html"><img src="../imag/ロゴ.jpg" width=180px; alt="ロゴ"></a></li>
+      <li>農園について</li>
+      <li><a href="../shop/shop_list.php">ご購入はこちら</a></li>
+      <li>調理例</li>
+      <li>お知らせ</li>
+      <li>お問い合わせ</li>
+    </ul>
+  </nav>
